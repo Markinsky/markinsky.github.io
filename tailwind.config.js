f037fc78efef0a6bf3b65a1cdadfb2751db329fd
Markinsky <carlosgerardo1957@hotmail.com>
2023-05-30 20:40:47 -0600

    Creación del repositorio

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "*.{html,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'bgColor': '#1c2826',
+        'bgColorB': '#273633',
+        'labelColorHover': '#546965',
+        'labelColor': '#FDFFFC',
+        'cardsColor': '#5B2333',
+        'cardsLabel': '#CED0CE',
+      },
+      fontFamily:{
+        fontIBM: ['IBM Plex Sans'],
+        fontGrotesk: ['Schibsted Grotesk'],
+        fontHeebo: ['Heebo'],
+      },
+      fontWeight: {
+        'regular': 400,
+        'medium': 500,
+      }
+    },
+  },
+  plugins: [],
+}
+
